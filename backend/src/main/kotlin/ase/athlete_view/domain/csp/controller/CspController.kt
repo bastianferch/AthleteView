38d1c4cb8bb18a7e6306351742d6b62063ee828b
Domork <domork3@gmail.com>
2023-11-10 21:19:12 +0100

    feat #29: Added Csp domain structure.

---
+++ backend/src/main/kotlin/ase/athlete_view/domain/csp/controller/CspController.kt
@@ -0,0 +1,9 @@
+package ase.athlete_view.domain.csp.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("api/csp")
+@RestController
+class CspController {
+}
---
+++ backend/src/main/kotlin/ase/athlete_view/domain/csp/service/impl/CspServiceImpl.kt
@@ -0,0 +1,8 @@
+package ase.athlete_view.domain.csp.service.impl
+
+import ase.athlete_view.domain.csp.service.CspService
+import org.springframework.stereotype.Service
+
+@Service
+class CspServiceImpl: CspService {
+}
