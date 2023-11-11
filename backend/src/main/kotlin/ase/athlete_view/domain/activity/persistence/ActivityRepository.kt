edabc5e04bc9dc75f0499cdf825643140ea7b77b
Stephan Stöger <e11907549@student.tuwien.ac.at>
2023-11-11 20:22:29 +0100

    try configure mongodb
    repository not yet found as bean

--- backend/src/main/kotlin/ase/athlete_view/domain/activity/persistence/ActivityRepository.kt
+++ backend/src/main/kotlin/ase/athlete_view/domain/activity/persistence/ActivityRepository.kt
@@ -1,9 +1,14 @@
 package ase.athlete_view.domain.activity.persistence
 
 import ase.athlete_view.domain.activity.pojo.entity.Activity
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.stereotype.Repository
 
+// TODO: Fix Repo
 @Repository
-interface ActivityRepository : JpaRepository<Activity, Long> {
+//@Configuration
+interface ActivityRepository: MongoRepository<Activity, String> {
+//    fun findAllById(id: String)
+//    fun save(activity: Activity)
 }
