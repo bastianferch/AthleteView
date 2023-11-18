7d36e6a59ca45b8a9d6023d4388aaa6610962449
Dmytro Kondrashov <domork3@gmail.com>
2023-11-18 10:11:03 +0100

    feat #29: Fixed backend registration bug. Fixed lint issues.

--- frontend/src/app/domain/calendar/component/calendar.component.ts
+++ frontend/src/app/domain/calendar/component/calendar.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+  styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent {
 
