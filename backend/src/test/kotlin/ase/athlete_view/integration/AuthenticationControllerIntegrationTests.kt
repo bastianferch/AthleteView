15b191d64cadc3673f3420827be561e9b1d1a014
Stephan Stöger <e11907549@student.tuwien.ac.at>
2023-11-23 23:31:56 +0100

    move integrationtest to mockMvc due to transaction
    with restTemplate transactions do not propagate thus the handling thread didn't get access to any data.

--- backend/src/test/kotlin/ase/athlete_view/integration/AuthenticationControllerIntegrationTests.kt
+++ backend/src/test/kotlin/ase/athlete_view/integration/AuthenticationControllerIntegrationTests.kt
@@ -2,35 +2,48 @@ package ase.athlete_view.integration
 
 import ase.athlete_view.AthleteViewApplication
 import ase.athlete_view.domain.authentication.dto.LoginDTO
-import ase.athlete_view.domain.user.pojo.dto.UserDto
+import ase.athlete_view.domain.user.persistence.UserRepository
 import ase.athlete_view.util.TestBase
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 
 @SpringBootTest(
     classes = [AthleteViewApplication::class],
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@AutoConfigureMockMvc
 class AuthenticationControllerIntegrationTests: TestBase() {
     @Autowired
-    private lateinit var restTemplate: TestRestTemplate
+    private lateinit var restTemplate: MockMvc
+
+    @Autowired
+    private lateinit var ur: UserRepository
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
 
     @Test
     fun loginWithCorrectCredentials_ShouldReturnOk() {
         val login = LoginDTO("a@s.com", "asdf")
-        val result = restTemplate.postForEntity("/api/auth/login", login, UserDto::class.java)
-
-        assert(result != null)
-        assert(HttpStatus.OK == result?.statusCode)
-        assert(result?.hasBody() == true)
-        val dto = result?.body
-        assert(dto?.email == "a@s.com")
-        assert(dto?.password == "asdf")
-        assert(dto?.token != null)
-        assert(dto?.id != null)
+        restTemplate.perform(
+            post("/api/auth/login")
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8")
+                .content(objectMapper.writeValueAsString(login))
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.token").isNotEmpty)
     }
 }
