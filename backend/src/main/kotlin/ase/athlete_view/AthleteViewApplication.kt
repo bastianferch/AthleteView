b363c97c66b5de74dc324222734015192231ca3e
Stephan Stöger <e11907549@student.tuwien.ac.at>
2023-12-02 00:08:41 +0100

    feat #12: remove testcontainers, clean merged pom

--- backend/src/main/kotlin/ase/athlete_view/AthleteViewApplication.kt
+++ backend/src/main/kotlin/ase/athlete_view/AthleteViewApplication.kt
@@ -4,8 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-//@EnableMongoRepositories
-//@EnableMongoRepositories(basePackageClasses = [ FitDataRepository::class ])
 class AthleteViewApplication
 
 fun main(args: Array<String>) {
