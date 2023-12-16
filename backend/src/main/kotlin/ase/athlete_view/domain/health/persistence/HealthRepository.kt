834956d736f38d8309378ea55d37259eeac5dd93
Dmytro Kondrashov <domork3@gmail.com>
2023-12-16 20:59:15 +0100

    feat #12: Refactored mock method: added HealthMapper, checking if the health already exists with such a date.

--- backend/src/main/kotlin/ase/athlete_view/domain/health/persistence/HealthRepository.kt
+++ backend/src/main/kotlin/ase/athlete_view/domain/health/persistence/HealthRepository.kt
@@ -3,7 +3,10 @@ package ase.athlete_view.domain.health.persistence
 import ase.athlete_view.domain.health.pojo.entity.Health
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
+import java.util.*
 
 @Repository
 interface HealthRepository : JpaRepository<Health, Long> {
+    fun findByDate(date: LocalDate): Optional<Health>
 }
--- backend/src/main/kotlin/ase/athlete_view/domain/health/service/impl/HealthServiceImpl.kt
+++ backend/src/main/kotlin/ase/athlete_view/domain/health/service/impl/HealthServiceImpl.kt
@@ -1,12 +1,14 @@
 package ase.athlete_view.domain.health.service.impl
 
-import ase.athlete_view.common.exception.entity.ForbiddenException
+import ase.athlete_view.common.exception.entity.ConflictException
+import ase.athlete_view.common.exception.entity.InternalException
 import ase.athlete_view.common.user.UserUtils
 import ase.athlete_view.domain.health.persistence.HealthRepository
 import ase.athlete_view.domain.health.pojo.dto.HealthDTO
-import ase.athlete_view.domain.health.pojo.entity.Health
 import ase.athlete_view.domain.health.service.HealthService
+import ase.athlete_view.domain.health.service.mapper.HealthMapper
 import ase.athlete_view.domain.user.service.UserService
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
@@ -15,26 +17,25 @@ import org.springframework.web.client.RestTemplate
 class HealthServiceImpl(
     private val healthRepository: HealthRepository,
     private val restTemplate: RestTemplate,
-    private val userService: UserService
+    private val userService: UserService,
+    private val healthMapper: HealthMapper
 ) : HealthService {
+    val log = KotlinLogging.logger {}
+
     @Value("\${api.mock.url}")
     val apiHost: String? = null
+
     override fun mock() {
-        var answer: HealthDTO? = apiHost?.let { this.restTemplate.getForObject(it, HealthDTO::class.java) }
-            ?: throw ForbiddenException("You do not have access for this action.")
-        var userDTO = UserUtils.currentUser()
-        var user = userDTO.id?.let { this.userService.getById(it) }
-            ?: throw ForbiddenException("You do not have access for this action.")
-        if (answer != null) {
-            this.healthRepository.save(Health(
-                id = null,
-                user = user,
-                avgBPM = answer.avgBPM,
-                avgSleepDuration = answer.avgSleepDuration,
-                avgSteps = answer.avgSteps,
-                date = answer.date
-            ))
+        val user = UserUtils.currentUser().id?.let { this.userService.getById(it) }
+            ?: throw ConflictException("You do not have access for this action.")
+        val healthDTO: HealthDTO = apiHost?.let { this.restTemplate.getForObject(it, HealthDTO::class.java) }
+            ?: throw InternalException("Could not access the external API.")
+        if (healthRepository.findByDate(healthDTO.date).isPresent){
+            log.warn { "Could not save a mock health, since there is an existing Health with date ${healthDTO.date}." }
+            return;
         }
+        val health = this.healthMapper.toEntity(healthDTO, user)
+        this.healthRepository.save(health)
     }
 
 }
