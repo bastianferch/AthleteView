5c5414e9dba3a5fabdb8ae9dec0cee4c53e77bea
Bastian Ferch <tutor_bastian.ferch@inso.tuwien.ac.at>
2023-12-23 23:50:56 +0100

    fix #62: fixed pipeline

--- frontend/src/app/domain/invite-dialog/invite-dialog.component.ts
+++ frontend/src/app/domain/invite-dialog/invite-dialog.component.ts
@@ -1,26 +1,26 @@
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {Component, inject} from '@angular/core';
-import {MatChipEditedEvent, MatChipInputEvent} from '@angular/material/chips';
-import {LiveAnnouncer} from '@angular/cdk/a11y';
-import {MatDialogRef} from '@angular/material/dialog';
-import {UserService} from '../user/service/UserService';
-import {SnackbarService} from '../../common/service/snackbar.service';
+import { COMMA, ENTER, SPACE } from '@angular/cdk/keycodes';
+import { Component, inject } from '@angular/core';
+import { MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserService } from '../user/service/UserService';
+import { SnackbarService } from '../../common/service/snackbar.service';
 
 
 @Component({
   selector: 'app-invite-dialog',
   templateUrl: './invite-dialog.component.html',
-  styleUrls: ['./invite-dialog.component.scss']
+  styleUrls: ['./invite-dialog.component.scss'],
 })
 export class InviteDialogComponent {
   addOnBlur = true;
-  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  readonly separatorKeysCodes = [ENTER, COMMA, SPACE] as const;
   emails: string[] = [];
 
   announcer = inject(LiveAnnouncer);
 
   constructor(private dialogRef: MatDialogRef<InviteDialogComponent>, private userService: UserService,
-              private snackbarService: SnackbarService) {
+    private snackbarService: SnackbarService) {
   }
 
   add(event: MatChipInputEvent): void {
