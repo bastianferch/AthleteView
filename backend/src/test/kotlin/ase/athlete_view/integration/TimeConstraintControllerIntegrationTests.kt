5dc343c53b51d38bb793980916ec756126b71bd3
michael.strasser <mi.st.pf@gmail.com>
2023-12-27 19:46:05 +0100

    fix #60 fix comments in time constraint tests and remove '/' from path in datagen

--- backend/src/test/kotlin/ase/athlete_view/integration/TimeConstraintControllerIntegrationTests.kt
+++ backend/src/test/kotlin/ase/athlete_view/integration/TimeConstraintControllerIntegrationTests.kt
@@ -11,7 +11,6 @@ import ase.athlete_view.util.TestBase
 import ase.athlete_view.util.WithCustomMockUser
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import io.github.oshai.kotlinlogging.KotlinLogging
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
