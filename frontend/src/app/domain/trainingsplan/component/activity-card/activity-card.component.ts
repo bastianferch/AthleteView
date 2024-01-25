c61c14dae38c0cd456dc87de97ea4722109135c8
Florian Drucker <flow.drucka64@gmx.at>
2024-01-25 12:13:26 +0100

    style (#18): rework styling (mobile)

--- frontend/src/app/domain/trainingsplan/component/activity-card/activity-card.component.ts
+++ frontend/src/app/domain/trainingsplan/component/activity-card/activity-card.component.ts
@@ -8,37 +8,43 @@ import { StyleMapperService } from "../../../../common/service/style-mapper.serv
     <mat-card class="activity-card">
       <mat-card-content class="activity-card-content">
         <div class="colored-bar" [style.background-color]="getColorCode(activity)"></div>
-        <img class="preview"
-             [src]="getIconPath(activity)"
-             alt="placholder"
-        />
-        <div>
-          <div>{{ activity.name }} </div>
-          <div>{{activity.estimatedDuration}}</div>
-          <div>{{ activity.note}}</div>
-        </div>
-        <div>
-          <div *ngIf="activity.template">
-            <div>
-              <a>With Trainer?
-                <mat-checkbox class="checkbox"
-                              [(ngModel)]="activity.withTrainer"
-                              [disabled] = "!interActive"
-                              (change)="onCheckboxChange()"
-                              checked="{{activity.withTrainer}}">
 
-                </mat-checkbox>
-              </a>
+        <div class="activitycard-column">
+          <img class="activity-icon"
+               [src]="getIconPath(activity)"
+               alt="placholder"
+          />
+        </div>
+
+
+        <div class="activitycard-column">
+          <div class="activitycard-fat">{{ activity.name }} </div>
+          <div>{{activity.estimatedDuration}} min</div>
+        </div>
+
+        <div class="activitycard-column">
+          <div *ngIf="activity.template">
+              <div>Supervised?</div>
+              <mat-checkbox class="checkbox"
+                                [(ngModel)]="activity.withTrainer"
+                                [disabled]="!interActive"
+                                (change)="onCheckboxChange()"
+                                checked="{{activity.withTrainer}}">
+              </mat-checkbox>
             </div>
+        </div>
+
+        <div class="activitycard-column">
+          <div class="mover" *ngIf="activity.template && interActive">
+            <svg width="24px" fill="currentColor" viewBox="0 0 24 24">
+              <path
+                d="M10 9h4V6h3l-5-5-5 5h3v3zm-1 1H6V7l-5 5 5 5v-3h3v-4zm14 2l-5-5v3h-3v4h3v3l5-5zm-9 3h-4v3H7l5 5 5-5h-3v-3z"></path>
+              <path d="M0 0h24v24H0z" fill="none"></path>
+            </svg>
           </div>
         </div>
-        <div *ngIf="activity.template && interActive">
-          <svg width="24px" fill="currentColor" viewBox="0 0 24 24">
-            <path
-              d="M10 9h4V6h3l-5-5-5 5h3v3zm-1 1H6V7l-5 5 5 5v-3h3v-4zm14 2l-5-5v3h-3v4h3v3l5-5zm-9 3h-4v3H7l5 5 5-5h-3v-3z"></path>
-            <path d="M0 0h24v24H0z" fill="none"></path>
-          </svg>
-        </div>
+
+
       </mat-card-content>
     </mat-card>
   `,
